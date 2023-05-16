e090b29b699ef2d97a833b22884b9507697b38ab
Vefa Ozlu <vefaozlu01@gmail.com>
2023-05-16 03:32:37 +0300

    Port added to .env

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-const port = 4000
+require("dotenv").config();
+const port = process.env.PORT
 
 app.use(express.static("build"));
 
